2bd530d2d7f2ec94a50138b0dc8e73606e90c53f
Mikael Menezes <mixaelmenezes@gmail.com>
2025-12-29 07:53:57 -0300

    method post support

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use reqwest::Client;
+use reqwest::blocking::Client;
 use std::io::Write;
-
 fn main() {
     print!("digite o link principal da aplicação: ");
-    let mut default_link = String::new();
-    std::io::stdout().flush();
-    std::io::stdin().read_line(&mut default_link).unwrap();
+    let mut rota = String::new();
+    let _ = std::io::stdout().flush();
+    std::io::stdin().read_line(&mut rota).unwrap();
     println!();
 
     let mut stri = String::new();
@@ -16,10 +15,40 @@ fn main() {
         \x1B[33m3\x1B[0m para \x1B[33mPUT\x1B[0m
         \x1B[35m4\x1B[0m para \x1B[35mDELETE\x1B[0m: "
     );
-    std::io::stdout().flush();
+    let _ = std::io::stdout().flush();
     std::io::stdin().read_line(&mut stri).unwrap();
 
     println!();
     let funcao: u8 = stri.trim().parse().unwrap();
     let client = Client::new();
+    let subrota = String::new();
+    print!("digite a subrota: ");
+    let _ = std::io::stdout().flush();
+    std::io::stdin().read_line(&mut rota).unwrap();
+    println!();
+    let retorno = match funcao {
+        1 => client
+            .get(rota + subrota.as_str())
+            .send()
+            .unwrap()
+            .text()
+            .unwrap(),
+        2 => {
+            print!("digite o json: ");
+            let mut jason = String::new();
+            let _ = std::io::stdout().flush();
+            std::io::stdin().read_line(&mut jason).unwrap();
+            let jason = jason.trim();
+            println!();
+            client
+                .post(rota + subrota.as_str())
+                .json(&serde_json::from_str::<serde_json::Value>(jason).unwrap())
+                .send()
+                .unwrap()
+                .text()
+                .unwrap()
+        }
+        _ => "erro".to_string(),
+    };
+    println!("{retorno}");
 }
